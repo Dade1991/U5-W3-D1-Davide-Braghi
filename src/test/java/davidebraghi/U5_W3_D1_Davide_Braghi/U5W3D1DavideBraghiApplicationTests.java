9e28a293a3b071515c7bd48abe4ec1df287ce5ca
Dade1991 <dade91@msn.com>
2025-10-20 14:49:55 +0200

    Initiliazed PJT folder

---
+++ src/test/java/davidebraghi/U5_W3_D1_Davide_Braghi/U5W3D1DavideBraghiApplicationTests.java
@@ -0,0 +1,13 @@
+package davidebraghi.U5_W3_D1_Davide_Braghi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D1DavideBraghiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
